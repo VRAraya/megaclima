c2a84e0e7d547d82ed9bf2a8923ea6c50838486f
VRAraya-olx <victor.araya@olxautos.com>
2021-08-02 02:06:21 -0400

    Agregados trycatchs a la vista de Clientes

--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -186,7 +186,11 @@ export default {
   },
   setup () {
     onMounted(() => {
-      clientService.value.getClients().then(data => { clients.value = data })
+      try {
+        clientService.value.getClients().then(data => { clients.value = data })
+      } catch (err) {
+        console.error(err)
+      }
     })
 
     const dt = ref()
@@ -224,12 +228,15 @@ export default {
         client.value.city = client.value.city.value ? client.value.city.value : client.value.city
         client.value.phone = client.value.phone.value ? client.value.phone.value : client.value.phone
         client.value.rut = client.value.rut.value ? client.value.rut.value : client.value.rut
-        clientService.value.createClient(client.value)
-          .then(data => { console.log(data) })
-          .finally(() => {
-            clientService.value.getClients().then(data => { clients.value = data })
-          })
-
+        try {
+          clientService.value.createClient(client.value)
+            .then(data => { console.log(data) })
+            .finally(() => {
+              clientService.value.getClients().then(data => { clients.value = data })
+            })
+        } catch (err) {
+          console.error(err)
+        }
         clientDialog.value = false
         client.value = {}
       }
@@ -246,15 +253,19 @@ export default {
     }
     // Elimina el cliente
     const deleteClient = (cliId) => {
-      clientService.value.deleteClient(cliId)
-        .then(data => {
-          console.log(data)
-        })
-        .finally(() => {
-          console.log('Cliente Eliminado')
-          clientService.value.getClients()
-            .then(data => { clients.value = data })
-        })
+      try {
+        clientService.value.deleteClient(cliId)
+          .then(data => {
+            console.log(data)
+          })
+          .finally(() => {
+            console.log('Cliente Eliminado')
+            clientService.value.getClients()
+              .then(data => { clients.value = data })
+          })
+      } catch (err) {
+        console.error(err)
+      }
       deleteClientDialog.value = false
       client.value = {}
     }
@@ -268,15 +279,23 @@ export default {
     }
     // Elimina los clientes selecionados
     const deleteSelectedClients = () => {
-      selectedClients.value.forEach(cli => {
-        clientService.value.deleteClient(cli.id)
-          .then(data => { console.log(data) })
-      })
+      try {
+        selectedClients.value.forEach(cli => {
+          clientService.value.deleteClient(cli.id)
+            .then(data => { console.log(data) })
+        })
+      } catch (err) {
+        console.error(err)
+      }
       deleteClientsDialog.value = false
       selectedClients.value = null
-      clientService.value.getClients()
-        .then(data => { clients.value = data })
-      console.log('Clientes Eliminados')
+      try {
+        clientService.value.getClients()
+          .then(data => { clients.value = data })
+        console.log('Clientes Eliminados')
+      } catch (err) {
+        console.error(err)
+      }
     }
 
     watchEffect(() => {
